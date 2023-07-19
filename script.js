5e88f2d99c5098e90c3bc4f6d01b20ca6c5de80f
Ch.Charmi Reddy <85749261+Charmireddy@users.noreply.github.com>
2023-07-19 10:48:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function flower1(){
+    document.getElementById("flower").src="https://www.ikea.com/in/en/images/products/smycka-artificial-flower-rose-red__0636963_pe698124_s5.jpg?f=xl";
+  }
+  function flower2(){
+      document.getElementById("flower").src="https://www.thespruce.com/thmb/p_UNZ5EDFNtNkNcHQA8wzmHLX-8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/plants-with-big-flowers-4138211-hero-b10becb169064cc4b3c7967adc1b22e1.jpg";
+    }
+    function flower3(){
+      document.getElementById("flower").src="https://www.1800flowers.com/blog/wp-content/uploads/2015/10/red-roses.jpg.webp";
+    }
